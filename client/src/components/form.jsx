7ec0b99d54035743cf5897275557e387708b7aa8
Janet Jiang <janetjiang1103@gmail.com>
2023-09-15 21:53:34 +0800

    Adding break in form

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -20,24 +20,29 @@ const FormEvent = (props) => {
     }
 
     return(
-        <form onSubmit={handleSubmit}>
+        <form  onSubmit={handleSubmit}>
             <h2> Please register your next Event in our community</h2>
 
             <label>Event Title</label>
             <input type="text" name="title" required placeholder="Title of your Event" ref={userTitle}/>
-
+            <br />
+            
             <label>Event Category</label>
             <input type="text" name="category" required placeholder="Category of your Event" ref={userCategory}/>
-
+            <br />
+            
             <label>Event Location</label>
             <input type="text" name="location" required placeholder="Where will be your event" ref={userLocation}/>
-
+            <br />
+            
             <label>Event Date</label>
             <input type="text" name="date" required placeholder="Date of your Event" ref={userDate}/>
-
+            <br />
+            
             <label>Favourite Event?</label>
             <input type="text" name="favourite" required placeholder="Please enter true or false" ref={userFavourite}/>
-
+            <br />
+            
             <button type="submit">Submit</button>
         </form>
 
