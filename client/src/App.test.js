804b3ffaa832a36bbd66aa6a3c9af338c59272a3
Janet Jiang <janetjiang1103@gmail.com>
2023-09-18 21:56:42 +0800

    remove test file

--- client/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from 'vitest';
-import Events from './components/events';
-
-test("Techtonica 2023 H2 events", () => {
-
-    const { getByText } = render(<Header />);
-    expect(getByText("Techtonica 2023 H2 events")).toBeDefined();
-
-});
